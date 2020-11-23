b5d6c5ca8a40156313eba44d951eabe9621fd376
jossrc <kuro.rc10@gmail.com>
2020-11-23 10:15:06 -0500

    Entendiendo sobre los tipos de datos

---
+++ D002_DataTypes/dataTypes.py
@@ -0,0 +1,27 @@
+#Data Types
+
+#String
+
+print("Hello"[0])
+
+print("Hello"[4])
+
+print("123" + "345")
+
+#Integer
+
+print(123 + 345)
+
+print(123_456_789)
+
+#Float
+
+pi = 3.14159
+print(pi)
+
+#Boolean
+
+answer1 = True
+answer2 = False
+
+print(answer1, answer2)
