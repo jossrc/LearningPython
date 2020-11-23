ef6c50e78044c13ff147b450f02d5128a4be538f
jossrc <kuro.rc10@gmail.com>
2020-11-23 11:14:07 -0500

    Resolviendo el segundo ejercicio

--- D002_DataTypes/exercise_02/main.py
+++ D002_DataTypes/exercise_02/main.py
@@ -4,3 +4,12 @@ weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+
+height_as_float = float(height)
+weight_as_int   = int(weight)
+
+bmi = weight_as_int / (height_as_float**2)
+
+bmi_as_int = int(bmi)
+
+print(bmi_as_int)
