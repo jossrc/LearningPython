5bcd0654b827b3da728a7bd0cad768d6c33d460f
jossrc <kuro.rc10@gmail.com>
2020-11-23 11:00:24 -0500

    Operaciones matemáticas

---
+++ D002_DataTypes/mathOperation.py
@@ -0,0 +1,37 @@
+# Suma
+
+1 + 3
+
+# Resta
+
+8 - 4
+
+# Multiplicación
+
+4 * 3
+
+# División
+
+6 / 3  # siempre -> float
+print(6/3) # 2.0
+
+6 // 3 # siempre -> int
+print(6//3)
+
+# Potencia
+
+2 ** 3
+print( 2**3 ) # 8
+
+'''
+ORDEN : PEMDASLR
+
+  -> ()
+  -> **
+  -> * /
+  -> + -
+'''
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
+print(3 - 3 + 3 / 3 * 3)
