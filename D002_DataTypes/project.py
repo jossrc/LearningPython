fad1f3603b367ccefbbc8e34e47acdce24c3532f
jossrc <kuro.rc10@gmail.com>
2020-11-27 18:05:37 -0500

    Resolviendo el proyecto

--- D002_DataTypes/project.py
+++ D002_DataTypes/project.py
@@ -3,4 +3,13 @@
 #Format the result to 2 decimal places = 33.60
 #Tip: You might need to do some research in Google to figure out how to do this.
 
+print("Welcome to the tip calculator.")
+total_bill = float( input("What was the total bill? $") )
+percentage = int( input("What percentage tip would you like to give? 10, 12, or 15? ") )
+number_people = int( input("How many people to split the bill? ") )
 
+tip = total_bill * percentage / 100
+
+total_pay = (total_bill + tip) / number_people
+
+print(f"Each person should pay: ${ round(total_pay, 2) }")
