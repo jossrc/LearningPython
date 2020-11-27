d057a1a0f1face48136c5817f6977711de229234
jossrc <kuro.rc10@gmail.com>
2020-11-27 17:12:30 -0500

    Manipulando numeros y entendiendo sobre los f-Strings

---
+++ D002_DataTypes/number.py
@@ -0,0 +1,26 @@
+
+print(round(2.666666666, 2))
+
+print(8 // 3)
+
+print(type(8 // 3))
+print(type(8 / 3))
+
+result = 4 / 2
+result /= 2
+print('The result is: ' + str(result))
+
+score = 0
+
+# User scores a point
+
+score += 1
+
+print('Your score is: ' + str(score))
+
+#f-String
+
+height = 1.8
+isWinning = True
+
+print(f'Your height is: {height}, your score is {score}, you are winning is {isWinning}')
