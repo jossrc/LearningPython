58cdc166d2c46fe145738b5fc12482274aa3e359
jossrc <kuro.rc10@gmail.com>
2020-12-02 13:53:52 -0500

    Resolviendo el primer ejercicio

--- D003_ControlFlow/exercise_01/main.py
+++ D003_ControlFlow/exercise_01/main.py
@@ -3,3 +3,8 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+
+if number % 2 == 0:
+    print('This is an even number.')    
+else:
+    print('This is an odd number.')
