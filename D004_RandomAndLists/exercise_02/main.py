53fb93cd9d35bbbd622095ea60af2b0b583a82a6
jossrc <kuro.rc10@gmail.com>
2020-12-12 09:56:25 -0500

    Resolviendo el segundo ejercicio

--- D004_RandomAndLists/exercise_02/main.py
+++ D004_RandomAndLists/exercise_02/main.py
@@ -1,6 +1,14 @@
+import random
+
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
+
+index_name = random.randint(0, len(names) - 1)
+
+who_pay = names[index_name]
+
+print(f"{who_pay} is going to buy the meal today!")
