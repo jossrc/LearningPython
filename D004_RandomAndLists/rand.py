17712fb204cad376bcfbc83ea71f68ac1cbceed7
jossrc <kuro.rc10@gmail.com>
2020-12-12 08:43:40 -0500

    Usando modulo random y personalizado

--- D004_RandomAndLists/rand.py
+++ D004_RandomAndLists/rand.py
@@ -1,4 +1,14 @@
+# Imports
 import random
+import my_module
 
-random_integer = random.randint(1,10)
-print(random_integer)
+random_integer = random.randint(1, 10)
+print(f"Random Integer : {random_integer}")
+
+random_float = random.random()
+print(f"Random Float : {random_float}")
+
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}")
+
+print(f"My Module (PI): {my_module.pi}")
