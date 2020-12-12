0c4cabc9d020dade41a6096c3aa809db9973de52
jossrc <kuro.rc10@gmail.com>
2020-12-12 10:38:17 -0500

    Resolviendo el tercer ejercicio

--- D004_RandomAndLists/exercise_03/main.py
+++ D004_RandomAndLists/exercise_03/main.py
@@ -11,7 +11,10 @@ position = input("Where do you want to put the treasure? ")
 
 # Write your code below this row 👇
 
+column = int(position[0])
+row = int(position[1])
 
+map[row - 1][column - 1] = "x"
 
 # Write your code above this row 👆
 
