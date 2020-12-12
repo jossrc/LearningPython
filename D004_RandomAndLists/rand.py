049d3d2f7f82101baa8909b2d5daa5726b081b4f
jossrc <kuro.rc10@gmail.com>
2020-12-12 09:11:57 -0500

    Aplicando formato

--- D004_RandomAndLists/rand.py
+++ D004_RandomAndLists/rand.py
@@ -8,7 +8,7 @@ print(f"Random Integer : {random_integer}")
 random_float = random.random()
 print(f"Random Float : {random_float}")
 
-love_score = random.randint(1,100)
+love_score = random.randint(1, 100)
 print(f"Your love score is {love_score}")
 
 print(f"My Module (PI): {my_module.pi}")
