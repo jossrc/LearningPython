8437d176e740719ea78920f173d660cad2aa006d
jossrc <kuro.rc10@gmail.com>
2020-12-14 07:45:53 -0500

    Resolviendo el proyecto del día 05

--- D005_Loops/project.py
+++ D005_Loops/project.py
@@ -12,9 +12,29 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Easy Level - Order not randomised:
-# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
-
 # Hard Level - Order of characters randomised:
 # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+password = []
+
+for nr in range(1, nr_letters + 1):
+    random_position = random.randint(1, len(letters) - 1)
+    password.append(letters[random_position])
+
+for nr in range(1, nr_symbols + 1):
+    random_position = random.randint(1, len(symbols) - 1)
+    password.append(symbols[random_position])
+
+for nr in range(1, nr_numbers + 1):
+    random_position = random.randint(1, len(numbers) - 1)
+    password.append(numbers[random_position])
+
+aux = ""
+
+for position in range(1, len(password)):
+    random_position = random.randint(1, len(password) - 1)
+    aux = password[position]
+    password[position] = password[random_position]
+    password[random_position] = aux
+
+print("".join(password))
