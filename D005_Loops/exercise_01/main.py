9599ed29bf280d1fe0396f833c61bdffd844d120
jossrc <kuro.rc10@gmail.com>
2020-12-14 07:00:45 -0500

    Resolviendo el primer ejercicio

--- D005_Loops/exercise_01/main.py
+++ D005_Loops/exercise_01/main.py
@@ -4,5 +4,15 @@ for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
 
-
 # Write your code below this row 👇
+
+total_height = 0
+number_of_students = 0
+
+for height in student_heights:
+    total_height += height
+    number_of_students += 1
+
+average = round(total_height / number_of_students)
+
+print(average)
