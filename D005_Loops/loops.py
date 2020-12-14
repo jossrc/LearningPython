985105f4d06f9e936d90553baf1ecb1e02e35d4b
jossrc <kuro.rc10@gmail.com>
2020-12-14 07:07:14 -0500

    Uso del range en un for

--- D005_Loops/loops.py
+++ D005_Loops/loops.py
@@ -5,3 +5,17 @@ for fruit in fruits:
     print(fruit + " Pie")
 
 print(fruits)
+
+# For Loop with Range
+
+for number in range(1, 10):  # [1;10>
+    print(number)
+
+for number in range(1, 10, 3):  # + 3
+    print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)
