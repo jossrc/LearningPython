1987ea4da2a348ca09550d2ff354fb90f8041bbd
jossrc <kuro.rc10@gmail.com>
2020-12-14 07:09:46 -0500

    Resolviendo el tercer ejercicio

--- D005_Loops/exercise_03/main.py
+++ D005_Loops/exercise_03/main.py
@@ -1,1 +1,18 @@
 # Write your code below this row 👇
+
+# Method 1
+total = 0
+
+for number in range(1, 101):
+    if number % 2 == 0:
+        total += number
+
+print(total)
+
+# Method 2
+total2 = 0
+
+for number in range(2, 101, 2):
+    total2 += number
+
+print(total2)
