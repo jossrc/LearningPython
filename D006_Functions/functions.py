1db2520f2f9f5cf82a005ebe7a9fe24b9f129904
jossrc <kuro.rc10@gmail.com>
2020-12-14 10:22:27 -0500

    Jugando con Reeborg's World

---
+++ D006_Functions/functions.py
@@ -0,0 +1,26 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+
+# Create Functions
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
+
+
+# Reeborg's World
+
+def draw_square():
+    turn_left()
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
