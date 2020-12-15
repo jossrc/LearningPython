ad85e32470815ea8b9617decd1f3aa19594db396
jossrc <kuro.rc10@gmail.com>
2020-12-15 11:33:20 -0500

    Resolviendo el segundo paso

--- D007_Hangman/main.py
+++ D007_Hangman/main.py
@@ -1,14 +1,31 @@
 import random
 
-# Step 1
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
+print(f"Psst, the solution is {chosen_word}")
+
+display = []
+word_length = len(chosen_word)
+
+for _ in range(word_length):
+    display.append("_")
+
 guess = input("Guess a letter: ").lower()
+exists = False
 
 for letter in chosen_word:
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        exists = True
+        break
+
+if exists:
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = guess
+else:
+    print("Wrong !!")
+
+print(display)
