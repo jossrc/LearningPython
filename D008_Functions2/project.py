11cdd0486f0c9542ebf59e42c523741d7042847b
jossrc <kuro.rc10@gmail.com>
2020-12-16 15:18:58 -0500

    Creando metodo para encriptar

--- D008_Functions2/project.py
+++ D008_Functions2/project.py
@@ -1,9 +1,26 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z']
+            'v', 'w', 'x', 'y', 'z'] # 25 - 26
+
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        index = alphabet.index(letter)
+        new_index = index + shift_amount
+
+        if new_index >= len(alphabet):
+            new_index = abs(len(alphabet) - new_index)
+
+        new_letter = alphabet[new_index]
+        cipher_text += new_letter
+    print(cipher_text)
+
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+encrypt(text, shift)
 
 # TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
