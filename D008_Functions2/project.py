951b0d16301c21d463202d658a14df105c9c9c77
jossrc <kuro.rc10@gmail.com>
2020-12-21 10:23:26 -0500

    Terminando el proyecto

--- D008_Functions2/project.py
+++ D008_Functions2/project.py
@@ -37,18 +37,3 @@ while should_continue:
     if result == "no":
         should_continue = False
         print("Goodbye")
-
-# TODO-1: Import and print the logo from art.py when the program starts.
-
-# TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-# Hint: Think about how you can use the modulus (%).
-
-# TODO-3: What happens if the user enters a number/symbol/space?
-# Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-# e.g. start_text = "meet me at 3"
-# end_text = "•••• •• •• 3"
-
-# TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-# Hint: Try creating a new function that calls itself if they type 'yes'.
