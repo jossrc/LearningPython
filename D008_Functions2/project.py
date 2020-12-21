087ec7a81291cf04fad5b1ea5d2e25be4dcb87ec
jossrc <kuro.rc10@gmail.com>
2020-12-21 09:11:05 -0500

    Creando metodo para desencriptar

--- D008_Functions2/project.py
+++ D008_Functions2/project.py
@@ -1,5 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z']  # 25 - 26
+            'v', 'w', 'x', 'y', 'z']
 
 
 def encrypt(plain_text, shift_amount):
@@ -13,14 +13,31 @@ def encrypt(plain_text, shift_amount):
 
         new_letter = alphabet[new_index]
         cipher_text += new_letter
-    print(cipher_text)
+    print(f"The encoded text is {cipher_text}")
+
+
+def decrypt(cipher_text, shift_amount):
+    decoded_text = ""
+    for letter in cipher_text:
+        index = alphabet.index(letter)
+        original_index = index - shift_amount
+        original_letter = alphabet[original_index]
+        decoded_text += original_letter
+
+    print(f"The decoded text is {decoded_text}")
 
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-encrypt(text, shift)
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(cipher_text=text, shift_amount=shift)
+else:
+    print("Thank you :)")
+
 
 # TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
@@ -30,5 +47,5 @@ encrypt(text, shift)
 
 
 # TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call
-#  the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND*
+#  the correct function based on that 'direction' variable. You should be able to test the code to encrypt *AND*
 #  decrypt a message.
