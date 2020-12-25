82f4f2e7d40d75c297c957f223faecf3e89d0cb1
jossrc <kuro.rc10@gmail.com>
2020-12-25 14:14:04 -0500

    Entendiendo sobre listas y diccionarios anidados

--- D009_Dictionaries/dictionaries.py
+++ D009_Dictionaries/dictionaries.py
@@ -28,4 +28,36 @@ for key, value in my_dictionary.items():
 my_dictionary = {}
 print(my_dictionary)
 
+# Nesting
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
 
+# Nesting a List in a Dictionary
+
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    }
+}
+
+# Nesting Dictionary in a List
+
+travel_log_arr = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    }
+]
