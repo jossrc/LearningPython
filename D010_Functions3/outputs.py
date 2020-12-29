606fabc2136d43a7b81a9ac51064810eae4bfa0d
jossrc <kuro.rc10@gmail.com>
2020-12-29 10:13:03 -0500

    Uso de multiples return

--- D010_Functions3/outputs.py
+++ D010_Functions3/outputs.py
@@ -1,10 +1,17 @@
 # Functions with Outputs
 
 def format_name(f_name, l_name):
+
+    if f_name.strip() == "" or l_name.strip() == "":
+        return "You didn't provide valid inputs."  # Be careful, return -> None
+
     formatted_f_name = f_name.strip().title()
     formatted_l_name = l_name.strip().title()
 
     return f"{formatted_f_name} {formatted_l_name}"
 
 
-print(format_name("JoSE", "ROBles"))
+name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+print(f"Result : {format_name(name, last_name)}")
