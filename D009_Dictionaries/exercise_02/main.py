437d3f432120f679cf972ca81cd52b77b40c1fdd
jossrc <kuro.rc10@gmail.com>
2020-12-29 08:17:00 -0500

    Resolviendo el segundo ejercicio

--- D009_Dictionaries/exercise_02/main.py
+++ D009_Dictionaries/exercise_02/main.py
@@ -10,10 +10,19 @@ travel_log = [
         "cities": ["Berlin", "Hamburg", "Stuttgart"]
     },
 ]
+
+
 # 🚨 Do NOT change the code above
 
-# TODO: Write the function that will allow new countries
 # To be added to the travel_log. 👇
+
+def add_new_country(country_visited, times_visited, cities_visited):
+    new_country = {
+        "country": country_visited,
+        "visits": times_visited,
+        "cities": cities_visited
+    }
+    travel_log.append(new_country)
 
 
 # 🚨 Do not change the code below
