bdd50739634d08bf890b341ca020a17e07d1457c
jossrc <kuro.rc10@gmail.com>
2020-12-30 08:58:17 -0500

    Avanzando con la indicación 4 y 5

--- D011_Blackjack/main.py
+++ D011_Blackjack/main.py
@@ -1,3 +1,5 @@
+import random
+
 # ############## Blackjack Project ####################
 
 # Difficulty Normal 😎: Use all Hints below to complete the project.
@@ -35,9 +37,27 @@
 # 11 is the Ace.
 # cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+
+def deal_card():
+    """
+      Returns a random card from the deck.
+    """
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
 # Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 # user_cards = []
 # computer_cards = []
+
+NUMBER_OF_STARTING_CARDS = 2
+user_cards = []
+computer_cards = []
+
+for _ in range(NUMBER_OF_STARTING_CARDS):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
 # Hint 6: Create a function called calculate_score() that takes a List of cards as input
 # and returns the score.
