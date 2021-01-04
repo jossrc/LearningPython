cfe5046271d9d902d7f70c1c8a7e0bc949727697
jossrc <kuro.rc10@gmail.com>
2021-01-04 12:46:00 -0500

    Construyendo objeto de la clase Turtle y Screen

---
+++ D016_POOI/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+
+timmy.shape("turtle")
+timmy.color("DeepSkyBlue2")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
