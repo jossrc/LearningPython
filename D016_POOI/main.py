88193a5975ae8fdd5b6e1295efb5d458b7c84696
jossrc <kuro.rc10@gmail.com>
2021-01-04 13:22:20 -0500

    Trabajando con PrettyTable para crear tablas en consola

--- D016_POOI/main.py
+++ D016_POOI/main.py
@@ -1,4 +1,4 @@
-import prettytable
+from prettytable import PrettyTable
 from turtle import Turtle, Screen
 
 timmy = Turtle()
@@ -10,5 +10,14 @@ timmy.forward(100)
 
 my_screen = Screen()
 print(my_screen.canvheight)
+
+table = PrettyTable()
+
+table.field_names = ["Pokemon Name", "Type"]
+table.add_row(["Pikachu", "Electric"])
+table.add_row(["Squirtle", "Water"])
+table.add_row(["Charmander", "Fire"])
+
+print(table)
 
 my_screen.exitonclick()
