a2dc3cc1cabbae3cb4084c72df94347d354fe29d
jossrc <kuro.rc10@gmail.com>
2021-01-07 12:25:14 -0500

    Uso de atributos y el constructor

--- D017_POOII/main.py
+++ D017_POOII/main.py
@@ -1,5 +1,16 @@
 class User:
-    pass
+
+    def __init__(self, user_id, username, followers):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        print("new user being created...")
 
 
-user_1 = User()
+user_1 = User("001", "joss", 0)
+
+print(user_1.username)
+
+user_2 = User("002", "angela", 0)
+
+print(user_2)
