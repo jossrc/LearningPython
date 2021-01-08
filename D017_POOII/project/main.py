c52a5e41b3727fc8aadd30611213de410191bf41
jossrc <kuro.rc10@gmail.com>
2021-01-08 10:18:48 -0500

    Avanzando con el final del proyecto

--- D017_POOII/project/main.py
+++ D017_POOII/project/main.py
@@ -13,4 +13,7 @@ for question in question_data:
 game = QuizBrain(question_bank)
 
 while game.still_has_questions():
-    answer = game.next_question()
+    game.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {game.score}/{game.question_number}")
