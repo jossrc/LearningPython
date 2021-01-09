f7ed69d6c28e6c7552f5a134a77ac0521b8d9a93
jossrc <kuro.rc10@gmail.com>
2021-01-09 13:05:20 -0500

    Creando método para determinar el ángulo de un polígono

--- D018_GUI/exercise.py
+++ D018_GUI/exercise.py
@@ -1,4 +1,14 @@
 import turtle as t
 
 tim = t.Turtle()
+tim.shape("turtle")
+tim.color("blue")
+
+
+def calculate_interior_angle(number_of_sides: int):
+    return 180*(number_of_sides - 2) / number_of_sides
+
+
+screen = t.Screen()
+screen.exitonclick()
 
