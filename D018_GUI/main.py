868b7f972878028721b1ffb1cc9a6acdb5310816
jossrc <kuro.rc10@gmail.com>
2021-01-09 09:08:14 -0500

    Dibujando un cuadrado

--- D018_GUI/main.py
+++ D018_GUI/main.py
@@ -1,12 +1,23 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
+timmy = Turtle()
 
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(45)
-timmy_the_turtle.forward(100)
+timmy.shape("turtle")
+timmy.color("red")
+
+
+# Draw a Square
+
+def move_turtle(turtle: Turtle, angle: float, steps: int):
+    turtle.forward(steps)
+    turtle.right(angle)
+
+
+SIDES_OF_A_SQUARE = 4
+
+
+for _ in range(SIDES_OF_A_SQUARE):
+    move_turtle(timmy, 90, 100)
 
 screen = Screen()
 screen.exitonclick()
