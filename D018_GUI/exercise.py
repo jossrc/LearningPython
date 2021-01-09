42d78492a0d7a78ebdedeeda7196621c386344f0
jossrc <kuro.rc10@gmail.com>
2021-01-09 13:16:00 -0500

    Creando metodo para mover la tortuga y dibujar el polígono

--- D018_GUI/exercise.py
+++ D018_GUI/exercise.py
@@ -9,6 +9,19 @@ def calculate_interior_angle(number_of_sides: int):
     return 180*(number_of_sides - 2) / number_of_sides
 
 
+def move_turtle(turtle: t.Turtle, steps: int, angle: float):
+    turtle.forward(steps)
+    turtle.right(angle)
+
+
+def draw_polygon(turtle, number_of_sides):
+    for _ in range(number_of_sides):
+        angle_of_polygon = calculate_interior_angle(number_of_sides)
+        move_turtle(turtle, 200, angle_of_polygon)
+
+
+draw_polygon(tim, 6)
+
 screen = t.Screen()
 screen.exitonclick()
 
