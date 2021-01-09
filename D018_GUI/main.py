623e1d9b7100b4686e4bc017c05172df79bb629c
jossrc <kuro.rc10@gmail.com>
2021-01-09 09:22:23 -0500

    Usando alias en el import

--- D018_GUI/main.py
+++ D018_GUI/main.py
@@ -1,6 +1,6 @@
-from turtle import Turtle, Screen
+import turtle as t
 
-timmy = Turtle()
+timmy = t.Turtle()
 
 timmy.shape("turtle")
 timmy.color("red")
@@ -8,7 +8,7 @@ timmy.color("red")
 
 # Draw a Square
 
-def move_turtle(turtle: Turtle, angle: float, steps: int):
+def move_turtle(turtle, angle, steps):
     turtle.forward(steps)
     turtle.right(angle)
 
@@ -19,5 +19,5 @@ SIDES_OF_A_SQUARE = 4
 for _ in range(SIDES_OF_A_SQUARE):
     move_turtle(timmy, 90, 100)
 
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
