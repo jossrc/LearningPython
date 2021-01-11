64ed3912e8ca80f5a7c302f625f5e34adadd0cad
jossrc <kuro.rc10@gmail.com>
2021-01-11 13:15:22 -0500

    Resolviendo el proyecto

--- D018_GUI/Project/main.py
+++ D018_GUI/Project/main.py
@@ -17,9 +17,28 @@ def extract_colors_from(img: str, count: int):
 rgb_color_list = extract_colors_from('image.jpg', 30)
 
 
-def draw_dashed_line(turtle: t.Turtle, space: float, steps: int):
+def draw_dotted_line(turtle: t.Turtle, space: float, steps: int):
     for _ in range(steps):
         turtle.dot(20, random.choice(rgb_color_list))
         turtle.penup()
         turtle.forward(space)
 
+
+def draw_dotted_square(turtle: t.Turtle, space: float, number_of_points_per_side: int):
+    for _ in range(number_of_points_per_side):
+        draw_dotted_line(turtle, space, number_of_points_per_side)
+        turtle.backward(space*number_of_points_per_side)
+        turtle.left(90)
+        turtle.forward(space)
+        turtle.right(90)
+
+
+t.colormode(255)
+timmy = t.Turtle()
+timmy.hideturtle()
+timmy.speed("fastest")
+
+draw_dotted_square(timmy, 30, 10)
+
+screen = t.Screen()
+screen.exitonclick()
