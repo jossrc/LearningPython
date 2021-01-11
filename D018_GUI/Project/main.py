bc9efd72e1f5a9e0f31ae46089d3d1de060858c1
jossrc <kuro.rc10@gmail.com>
2021-01-11 11:37:50 -0500

    Creando metodo para obtener lsita de colores en rgb

--- D018_GUI/Project/main.py
+++ D018_GUI/Project/main.py
@@ -1,2 +1,17 @@
 import colorgram
 
+
+def extract_colors_from(img: str, count: int):
+    rgb_colors: list[tuple[int, int, int]] = []
+    colors = colorgram.extract(img, count)
+    for color in colors:
+        r = color.rgb.r
+        g = color.rgb.g
+        b = color.rgb.b
+        rgb_colors.append((r, g, b))
+    return rgb_colors
+
+
+rgb_color_list = extract_colors_from('image.jpg', 30)
+
+print(rgb_color_list)
