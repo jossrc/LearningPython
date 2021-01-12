6f02af6a7227f37f790dfa75265c8d01aee0fdc9
jossrc <kuro.rc10@gmail.com>
2021-01-12 10:20:48 -0500

    Usando eventos para hacer figuras

--- D019_Higher_Order_Functions_And_Events/main.py
+++ D019_Higher_Order_Functions_And_Events/main.py
@@ -8,7 +8,32 @@ def move_forwards():
     tim.forward(10)
 
 
-screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+
+
+screen.listen()  # Permite estar atento a los eventos
+
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear)
 
 screen.exitonclick()
