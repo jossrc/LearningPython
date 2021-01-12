8b7e8f7fdba35366b50251f60218730fc632ddd7
jossrc <kuro.rc10@gmail.com>
2021-01-12 12:41:33 -0500

    Resolviendo el proyecto de carrera de tortugas

--- D019_Higher_Order_Functions_And_Events/Project/main.py
+++ D019_Higher_Order_Functions_And_Events/Project/main.py
@@ -1,24 +1,39 @@
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
 
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-
 colors = ["red", "orange", "pink", "green", "blue", "purple"]
 racers: dict[str, Turtle] = {}
+is_race_on = False
 
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+print(user_bet)
 
-def get_ready(starting_colors: list[str]):
-    y = -100
-    for color in starting_colors:
-        racers[color] = Turtle(shape="turtle")
-        racers[color].color(color)
-        racers[color].penup()
-        racers[color].goto(-230, y)
-        y += 40
+if user_bet:
+    is_race_on = True
 
+x = -230
+y = -100
+for color in colors:
+    racers[color] = Turtle(shape="turtle")
+    racers[color].color(color)
+    racers[color].penup()
+    racers[color].goto(x, y)
+    y += 40
 
-get_ready(colors)
+while is_race_on:
+    for turtle in racers.values():
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! the {winning_color} turtle is the winner!")
+            break
+        rand_distance = random.randint(1, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
