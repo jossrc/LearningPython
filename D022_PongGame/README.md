ca7b77e777c8d19a970e35937a93dd557cd7296d
jossrc <kuro.rc10@gmail.com>
2021-01-20 10:13:08 -0500

    Iniciando el día 22

---
+++ D022_PongGame/README.md
@@ -0,0 +1,3 @@
+## Learning Python 🐍 👨🏻‍💻
+
+- **Day 022** : *Build Pong*: The Famous Arcade Game
---
+++ D022_PongGame/main.py
@@ -0,0 +1,9 @@
+from turtle import Screen
+
+screen = Screen()
+screen.title("Pong Game")
+screen.setup(height=600, width=800)
+screen.bgcolor("black")
+
+screen.exitonclick()
+
