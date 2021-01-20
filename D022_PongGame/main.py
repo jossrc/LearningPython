121071a8186ec0a347e2e242e60ee1afe30f67ab
jossrc <kuro.rc10@gmail.com>
2021-01-20 12:33:51 -0500

    Detectando cuando la pelota llega al arco

--- D022_PongGame/main.py
+++ D022_PongGame/main.py
@@ -38,5 +38,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
         ball.bounce_x()
 
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect L paddle mises
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
 
