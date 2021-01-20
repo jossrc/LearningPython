6d0a9bc14938e9bd096ee651c64c41cc813cd0ff
jossrc <kuro.rc10@gmail.com>
2021-01-20 10:51:17 -0500

    Creando clase para la paleta

---
+++ D022_PongGame/paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, x_y: tuple[float, float]):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(x_y)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
