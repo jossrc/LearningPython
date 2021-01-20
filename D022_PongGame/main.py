f43204e52fa2d4c108e2fb334f2f3e0eefd3a8a9
jossrc <kuro.rc10@gmail.com>
2021-01-20 13:35:11 -0500

    Implementando el puntaje en el juego y corrigiendo bugs

--- D022_PongGame/main.py
+++ D022_PongGame/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 import time
 
@@ -17,6 +18,9 @@ l_paddle = Paddle((-350, 0))
 # Ball
 ball = Ball()
 
+# Scoreboard
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
@@ -26,7 +30,7 @@ screen.onkey(r_paddle.go_down, "Down")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -35,16 +39,18 @@ while game_is_on:
         ball.bounce_y()
 
     # Detect collision with paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or \
+       ball.distance(l_paddle) < 50 and ball.xcor() < -340:
         ball.bounce_x()
 
     # Detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect L paddle mises
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
-
