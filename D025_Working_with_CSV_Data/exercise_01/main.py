abdd64cc55d235b9c36b14e22ba7027b75e117f9
jossrc <kuro.rc10@gmail.com>
2021-01-23 14:13:25 -0500

    Resolviendo el ejercicio con el metodo de angela

--- D025_Working_with_CSV_Data/exercise_01/main.py
+++ D025_Working_with_CSV_Data/exercise_01/main.py
@@ -1,6 +1,24 @@
 import pandas
 
 data = pandas.read_csv("data.csv")
-grey_squirrels = data[data["Primary Fur Color"] == "Grey"]
-print(grey_squirrels)
 
+grey_squirrels = data[data["Primary Fur Color"] == "Gray"]
+grey_squirrels_count = len(grey_squirrels)
+
+red_squirrels = data[data["Primary Fur Color"] == "Cinnamon"]
+red_squirrels_count = len(red_squirrels)
+
+black_squirrels = data[data["Primary Fur Color"] == "Black"]
+black_squirrels_count = len(black_squirrels)
+
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_counts.csv")
