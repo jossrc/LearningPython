1c0967994195f7c2c4861569008eab9bb817471f
jossrc <kuro.rc10@gmail.com>
2021-01-23 11:52:12 -0500

    Agregando apuntes del paquete csv

--- D025_Working_with_CSV_Data/main.py
+++ D025_Working_with_CSV_Data/main.py
@@ -1,7 +1,14 @@
 import csv
 
+"""
+    El paquete csv permite trabajar de manera sencilla los archivos de ese tipo,
+    ya que cuenta con métodos que permiten leerlos. El método `reader()`
+    retorna un objeto de tipo reader, el cual al ser trabajado en un ciclo for
+    obtendremos la fila como una lista de string
+"""
+
 with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)  # object csv.reader
+    data = csv.reader(data_file)
     temperatures = []
     for row in data:
         if row[1] != "temp":
